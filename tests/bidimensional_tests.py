8f674d97ebb071d6ff5a5dcae9ee395e28b25538
ihavenonickname <gabrielblanksm@gmail.com>
2019-07-22 14:06:46 -0300

    trapezium tests

--- tests/bidimensional_tests.py
+++ tests/bidimensional_tests.py
@@ -1,1 +1,37 @@
-import sv_geometry
+import unittest
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from sv_geometry import bidimensional, SVGeometryException
+
+class Trapezium(unittest.TestCase):
+    def test_split_in_half(self):
+        result = bidimensional.split_trapezium(20, 10, 20, 10)
+
+        self.assertEqual(result, {
+            'upper': { 'upper_radius': 10, 'lower_radius': 15, 'H': 10 },
+            'lower': { 'upper_radius': 15, 'lower_radius': 20, 'H': 10 },
+        })
+
+    def test_fail_when_h_too_high(self):
+        with self.assertRaises(SVGeometryException):
+            bidimensional.split_trapezium(1, 1, 2, 3)
+
+    def test_fail_when_radius_is_negative(self):
+        with self.assertRaises(SVGeometryException):
+            bidimensional.split_trapezium(-1, 1, 2, 1)
+
+        with self.assertRaises(SVGeometryException):
+            bidimensional.split_trapezium(1, -1, 2, 1)
+
+    def test_fail_with_height_is_negative(self):
+        with self.assertRaises(SVGeometryException):
+            bidimensional.split_trapezium(1, 1, -2, 1)
+
+        with self.assertRaises(SVGeometryException):
+            bidimensional.split_trapezium(1, 1, 2, -1)
+
+if __name__ == '__main__':
+    unittest.main()
